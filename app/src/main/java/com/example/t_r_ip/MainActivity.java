55a5acc99608083e22bf487c329c133eae60cdd1
amittes <amitt886@gmail.com>
2023-01-20 11:52:07 +0200

    feat: creating login screen and logic & creating registration screen snd logic

--- app/src/main/java/com/example/t_r_ip/MainActivity.java
+++ app/src/main/java/com/example/t_r_ip/MainActivity.java
@@ -1,21 +1,9 @@
 package com.example.t_r_ip;
 
-import static android.content.ContentValues.TAG;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.t_r_ip.model.FirebaseModel;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseUIActivity firebaseUIActivity = new FirebaseUIActivity();
-
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
