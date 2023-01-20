a600e3a6b4d36dfa47b5a0aa785f5af7bbbf05ee
talshuker <talshuker11@gmail.com>
2023-01-20 18:25:43 +0200

    Feat: adding bottom bar menu

--- app/src/main/java/com/example/t_r_ip/MainActivity.java
+++ app/src/main/java/com/example/t_r_ip/MainActivity.java
@@ -10,6 +10,9 @@ import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     NavController navController;
@@ -21,13 +24,16 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_navhost);
         navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+        BottomNavigationView navView = findViewById(R.id.main_bottomNavigationView);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             navController.popBackStack();
-        } else if (item.getItemId() == R.id.main_menu_setting) {
+        } else if (item.getItemId() == R.id.main_menu_addPost) {
             navController.navigate(R.id.action_postsListFragment_to_postFragment);
         }
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/example/t_r_ip/UserProfileFragment.java
@@ -0,0 +1,44 @@
+package com.example.t_r_ip;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.MenuProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Lifecycle;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentActivity parentActivity = getActivity();
+        parentActivity.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menu.removeItem(R.id.main_menu_addPost);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        },this, Lifecycle.State.RESUMED);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        return view;
+    }
+}
